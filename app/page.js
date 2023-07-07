ba850f8b9aebe8feb0f9b82036b4916b30f75472
avargaskb <avargaskb@gmail.com>
2023-07-07 16:56:15 -0600

    Initial Commit

--- app/page.js
+++ app/page.js
@@ -1,113 +1,123 @@
-import Image from 'next/image'
+'use client';
+import { useState } from 'react';
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+	const Square = ({ value, onSquareClick }) => {
+		return (
+			<button className="square" onClick={onSquareClick}>
+				{value}
+			</button>
+		);
+	};
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+	const Board = () => {
+		const [squares, setSquares] = useState(Array(9).fill(null));
+		const [isXNext, setIsXNext] = useState(true);
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+		const handleClick = (i) => {
+			if (calculateWinner(squares) || squares[i]) {
+				return;
+			}
+			squares[i] = isXNext ? 'X' : 'O';
+			setSquares(squares);
+			setIsXNext(!isXNext);
+		};
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+		function calculateWinner(squares) {
+			const winnerPatterns = [
+				[0, 1, 2],
+				[3, 4, 5],
+				[6, 7, 8],
+				[0, 3, 6],
+				[1, 4, 7],
+				[2, 5, 8],
+				[2, 4, 6],
+				[0, 4, 8],
+			];
+			for (let i = 0; i < winnerPatterns.length; i++) {
+				const [a, b, c] = winnerPatterns[i];
+				if (
+					squares[a] &&
+					squares[a] === squares[b] &&
+					squares[a] === squares[c]
+				) {
+					return squares[a];
+				}
+			}
+			return null;
+		}
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
+		const winner = calculateWinner(squares);
+		let status;
+		if (squares.every((i) => i !== null)) {
+			status = 'Game over ';
+		} else if (winner) {
+			status = `Winner is ${winner}`;
+		} else {
+			status = `Is ${isXNext ? 'X' : 'O'} turn to play`;
+		}
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+		const handleReset = () => {
+			setSquares(Array(9).fill(null));
+			setIsXNext(true);
+		};
+
+		const ResetButton = () => {
+			return (
+				<div>
+					{squares.indexOf('X' || 'O') > -1 ? (
+						<button className="reset" onClick={handleReset}>
+							{' '}
+							Restart Game!
+						</button>
+					) : (
+						' '
+					)}
+				</div>
+			);
+		};
+
+		const renderSquare = (i) => {
+			return (
+				<Square
+					value={squares[i]}
+					onSquareClick={() => {
+						handleClick(i);
+					}}
+				/>
+			);
+		};
+		return (
+			<div className="container">
+				<div className='text-4xl font-bold my-10'>Tic Tac Toe</div>
+				<>
+					<div className="row">
+						{renderSquare(0)}
+						{renderSquare(1)}
+						{renderSquare(2)}
+					</div>
+					<div className="row">
+						{renderSquare(3)}
+						{renderSquare(4)}
+						{renderSquare(5)}
+					</div>
+					<div className="row">
+						{renderSquare(6)}
+						{renderSquare(7)}
+						{renderSquare(8)}
+					</div>
+				</>
+				<div className="status">{status}</div>
+				<div className="restart">
+					<ResetButton />
+				</div>
+			</div>
+		);
+	};
+
+	return (
+		<div>
+			<Board />
+		</div>
+	);
 }
